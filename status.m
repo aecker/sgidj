e14b537f919c139f2c5340d4e61f258aa2f63501
Alexander Ecker <alexander.ecker@uni-tuebingen.de>
2013-08-28 18:20:18 +0200

    Various fixes to status display

--- status.m
+++ status.m
@@ -7,32 +7,30 @@ function status(script)
 
 res = parse(script);
 tables = {};
-restrictions = {};
+restr = {};
+restrTxt = {};
 k = 0;
 for r = res
     switch r.cmd
         case 'table'
             k = k + 1;
             tables{k} = eval(r.arg); %#ok<*AGROW>
-            restrictions{k} = {};
+            restr{k} = {};
+            restrTxt{k} = '';
         case 'restr'
-            restrictions{k}{end + 1} = eval(r.arg);
+            restr{k}{end + 1} = eval(r.arg);
+            restrTxt{k} = [restrTxt{k} ' & ' r.arg];
     end
 end
 
 fprintf('\n')
 for i = 1 : k
-    if ~isempty(restrictions{i})
-        restr = sprintf(' & ''%s''', restrictions{i}{:});
-    else
-        restr = '';
-    end
-    total = count(tables{i}.popRel & restrictions{i});
-    populated = count(tables{i} & restrictions{i});
+    total = count(tables{i}.popRel & restr{i});
+    populated = count(tables{i} & restr{i});
     jobTable = eval([tables{i}.schema.package '.Jobs']);
     errors = count(jobTable & struct('status', 'error', 'table_name', class(tables{i})));
     running = count(jobTable & struct('status', 'reserved', 'table_name', class(tables{i})));
-    fprintf('Table %s%s\n', class(tables{i}), restr)
+    fprintf('Table %s%s\n', class(tables{i}), restrTxt{i})
     fprintf('  %d of %d populated (%.1f%%)\n', populated, total, populated / total * 100)
     fprintf('  %d errors\n', errors)
     fprintf('  %d running\n', running)
